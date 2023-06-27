6fd99bddc739a85e817a72417e8f51cce6691fa5
HosokawaR <thin.river.jp@gmail.com>
2023-06-28 00:58:56 +0900

    Fix search result minimizing

--- src/search.ts
+++ src/search.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { GCP_SEARCH_API_KEY, GOOGLE_CUSTOM_SEARCH_CX } from "./env.js";
-import { HTMLElement, parse } from "node-html-parser";
+import { JSDOM } from "jsdom";
 
 const GOOGLE_SEARCH_ENDPOINT = "https://www.googleapis.com/customsearch/v1";
 
@@ -29,28 +29,33 @@ const searchInGoogle = async (query: string): Promise<string> => {
 const fetchWebsiteContents = async (url: string): Promise<string> => {
 	const response = await axios(url);
 	const html = response.data;
-	const document = parse(html);
+	const document = new JSDOM(html).window.document;
 	const minimized = minimizeWebsiteContents(document);
 	return minimized;
 };
 
-const minimizeWebsiteContents = (document: HTMLElement): string => {
+const minimizeWebsiteContents = (document: Document): string => {
 	const text = extractTextFromDocument(document);
-	const minimized = text.replace(/\s+/g, " ");
+	const minimized = text
+		.split("\n")
+		.map((line) => line.trim())
+		.filter((line) => line.length > 0)
+		.join("\n")
+		.replace(/ +/g, " ")
+		.slice(0, 4000);
 	return minimized;
 };
 
-const extractTextFromDocument = (document: HTMLElement): string => {
-	const scriptNodes = Array.from(document.getElementsByTagName("script"));
-	for (let node of scriptNodes) {
-		node.parentNode.removeChild(node);
-	}
+const TAGS_TO_REMOVE = ["style", "script", "img", "iframe"];
 
-	const styleNodes = Array.from(document.getElementsByTagName("style"));
-	for (let node of styleNodes) {
-		node.parentNode.removeChild(node);
-	}
+const extractTextFromDocument = (document: Document): string => {
+	TAGS_TO_REMOVE.forEach((tag) => {
+		const targetNodes = [...document.getElementsByTagName(tag)];
+		for (let node of targetNodes) {
+			node.remove();
+		}
+	});
 
-	const text = document.innerText;
+	const text = document.body.textContent;
 	return text;
 };
